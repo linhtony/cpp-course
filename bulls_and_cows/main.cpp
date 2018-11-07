8d33ab4f5d7c54150865dff23893f7ae44023d0a
rinzlerr <duongquoctue@gmail.com>
2018-11-07 13:22:46 +0700

    fix digit check

--- bulls_and_cows/main.cpp
+++ bulls_and_cows/main.cpp
@@ -1,30 +1,72 @@
 #include <iostream>
+#include <ctime>
 #include <vector>
 
 
 using namespace std;
 
-int num_of_digits; // must be 0, 3, 4 or 5 (0 is the cheat code)
-void enterNumberOfDigits() {
-    cout << "Enter number of digits in code: ";
-    cin >> num_of_digits;
-}
+// number of digits.. must be 0, 3, 4 or 5 (0 is the cheat code)
+int num_of_digits; 
 
+
+// check if the user type the number of digits correctly
 bool checkNumberOfDigits() {
-    if (num_of_digits == 0 || num_of_digits == 3 || num_of_digits == 4 || num_of_digits == 5) { 
+    if ( (num_of_digits == 0) || (num_of_digits == 3) || (num_of_digits == 4) || (num_of_digits == 5) ) { 
         return true; 
     }
     else {
-        cout << "Invalid number of digits.." << endl;
+        cout << "Invalid number of digits" << endl;
         return false;
     }
 }
 
+
+
+
+
+
+// generate a random int with 'n' digit, only be called if num_of_digits not 0
+// @TODO: edit this function
+int randomInt(int digit) {
+    digit = num_of_digits;
+
+    while (true) {
+        srand(time(NULL));
+        int random_number = rand();
+        break;
+    }
+    return 0;
+}
+
+// called when num_of_digits is not 0
+// @TODO: edit this function
+int digitDefault(int digit) {
+
+    switch (digit) {
+
+    case (3):
+        break;
+    case (4):
+        break;
+    case (5):
+        break;
+    }
+
+}
+
+// called when the number of digits is 0
+// @TODO: edit this function
+int digitZero() {
+    int digit = num_of_digits; // 0 only 
+    return 0;
+}
+
 int main() 
 {   
+    // enter the number of digits and check for correctness
     while (true) {
-        enterNumberOfDigits();
-        checkNumberOfDigits();
+        cout << "Enter number of digits in code: ";
+        cin >> num_of_digits;
 
         if (checkNumberOfDigits() == true) {
         cout << "The number of digits is: " << num_of_digits << endl;
@@ -32,5 +74,10 @@ int main()
         }
     }
 
+
+
+
     // TODO: keep coding...
+    getchar();
+    getchar();
 }
