de6705f9d246da00830428a272a6f15f68409722
rinzlerr <duongquoctue@gmail.com>
2018-11-08 15:15:03 +0700

    num_of_digits = 0 case

--- bulls_and_cows/main.cpp
+++ bulls_and_cows/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
-#include <random>
+#include <algorithm>
 
 
 using namespace std;
@@ -20,6 +20,15 @@ bool checkNumberOfDigits() {
     }
 }
 
+// count the number of digits in an integer
+int digitsCount(int number) {
+    int digits = 0;
+    while (number) {
+    number /= 10;
+    digits++; 
+    }
+    return digits; 
+}
 
 int main() 
 {   
@@ -38,12 +47,60 @@ int main()
     }
 
 
-    // output the code (for testing purpose)
 
-    // TODO: add handler for num_of_digits = 0
+    // output the code (for testing purpose)
+    // TODO: add handler for num_of_digits = 0, when the guessed_num < num of digit (fill the first element with 0)
     if (num_of_digits == 0) {
-        cout << "Number to guess: " << endl;
-    } 
+        int manual_code;
+        int manual_code_count;
+        int manual_num_of_digits;
+        
+        cout << "Enter code: ";
+        cin >> manual_code;
+
+        manual_code_count = digitsCount(manual_code);
+
+        while (true) {
+            cout << "Enter number of digits in code: ";
+            cin >> manual_num_of_digits;
+            if (manual_num_of_digits == manual_code_count) {
+                int array[manual_num_of_digits];
+
+                for (int i = manual_num_of_digits - 1; i >= 0; i--) {
+                array[i] = manual_code % 10;
+                manual_code /= 10;
+                }
+                for (int i = 0; i <= manual_num_of_digits; i++) {
+                manualVector.push_back( array[i] );
+                }
+                // out the vector
+                for (int i = 0; i <= manual_code_count; i++) {
+                cout << manualVector[i];
+        }
+                break;
+            }
+            else if (manual_num_of_digits > manual_code_count) {
+                manualVector.push_back(0);
+                int array[manual_num_of_digits];
+                for (int i = 0; i <= manual_num_of_digits; i++) {
+                manualVector.push_back( array[i] );
+                }
+                // out the vector
+                for (int i = 0; i <= manual_code_count + 1; i++) {
+                cout << manualVector[i];
+                break;
+                } 
+            }
+            else if (manual_num_of_digits < manual_code_count) {
+                cout << "Invalid number of digits in code..! " << endl;
+            }
+        }
+
+        cout << manual_code_count;
+    }
+
+
+    
     else {
         srand(time(NULL));
         vector<int> rd = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
